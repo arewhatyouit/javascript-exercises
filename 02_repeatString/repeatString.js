aea2503570b472502f507d708552ccec7784c522
arewhatyouit <isaac.noyes@gmail.com>
2025-02-12 21:07:30 -0800

    Logic for test 1 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString = function(string, num) {
+    let result = ''
+    
+    for (i = 0; i < num; i++) {
+            result = result + string
+        }
+        return result;
+        }
+    
+    repeatString('hey', 3)
+    // repeatString('hello', 10)
+    
+    // Do not edit below this line
+    module.exports = repeatString;
+    
