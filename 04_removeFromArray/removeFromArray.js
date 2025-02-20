237421d882f8ddd2c15f4afc9300089172a7aa69
arewhatyouit <github@mail.noyesdesign.com>
2025-02-19 22:11:31 -0800

    finished 1-3, working on 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num) {
+const result = array.filter(v => v != num);
+return result
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
